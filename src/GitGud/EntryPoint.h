e6b4c6d9841e308867eca7b50d3b998ab8e8d367
Josef21296 <jcasanovasv@hotmail.com>
2020-04-01 22:32:34 +0200

    Added spdlog as submodule and basic logging system

--- src/GitGud/EntryPoint.h
+++ src/GitGud/EntryPoint.h
@@ -6,6 +6,10 @@ extern GitGud::Application* GitGud::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	GitGud::Log::Init();
+	GG_CORE_WARN("Init log!");
+	GG_INFO("dafuq {0}", 1);
+
 	auto app = GitGud::CreateApplication();
 	app->Run();
 	delete app;
