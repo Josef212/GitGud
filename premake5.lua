1895899a17ff94a63d1bd1a484bc1386254200f2
Josef21296 <jcasanovasv@hotmail.com>
2020-04-01 23:16:20 +0200

    Added premake build system for windows

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "GitGud"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GitGud"
+	location "GitGud"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "%/{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "%/{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GG_PLATFORM_WINDOWS",
+			"GG_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GG_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "%/{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "%/{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GitGud/vendor/spdlog/include",
+		"GitGud/src"
+	}
+
+	links
+	{
+		"GitGud"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GG_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GG_DIST"
+		optimize "On"
