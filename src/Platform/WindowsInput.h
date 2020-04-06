488f976de24a6200e82cbe13a5d63e1e3f33b387
Josef21296 <jcasanovasv@hotmail.com>
2020-04-06 21:41:58 +0200

    More input methods

--- src/Platform/WindowsInput.h
+++ src/Platform/WindowsInput.h
@@ -7,8 +7,14 @@ namespace GitGud
 	class WindowsInput : public Input
 	{
 	protected:
-		virtual bool IsKeyPressedImpl(int keyCode) override;
-		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual bool IsKeyImpl(int keyCode) override;
+		virtual bool IsKeyImplDown(int keyCode) override;
+		virtual bool IsKeyImplUp(int keyCode) override;
+
+		virtual bool IsMouseButtonImpl(int button) override;
+		virtual bool IsMouseButtonDownImpl(int button) override;
+		virtual bool IsMouseButtonUpImpl(int button) override;
+
 		virtual float GetMouseXImpl() override;
 		virtual float GetMouseYImpl() override;
 		virtual std::pair<float, float> GetMousePosImpl() override;
