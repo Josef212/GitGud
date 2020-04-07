40a3ba2835924b9f7d26c11217d9a10d6b766337
Josef21296 <jcasanovasv@hotmail.com>
2020-04-08 00:05:49 +0200

    Rendering a simple triangle

--- GitGud/src/GitGud/Application.cpp
+++ GitGud/src/GitGud/Application.cpp
@@ -21,6 +21,36 @@ namespace GitGud
 
 		_imguiLayer = new ImGuiLayer();
 		PushOverlay(_imguiLayer);
+
+		// ----------------------------
+
+		float vertices[3 * 3] =
+		{
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.0f,  0.5f, 0.0f
+		};
+
+		uint indices[] =
+		{
+			0, 1, 2
+		};
+
+		glGenVertexArrays(1, &_vertexArray);
+		glBindVertexArray(_vertexArray);
+
+		glGenBuffers(1, &_vertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (const void*)0);
+				
+		glGenBuffers(1, &_indexBuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		glBindVertexArray(0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	}
 
 	Application::~Application()
@@ -32,8 +62,12 @@ namespace GitGud
 	{
 		while (_running)
 		{
-			glClearColor(1, 0, 1, 1);
+			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			glBindVertexArray(_vertexArray);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, NULL);
 
 			for (Layer* layer : _layerStack)
 			{
