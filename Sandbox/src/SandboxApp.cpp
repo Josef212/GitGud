7b1b75f9597c65ac8c74c366890ab61bb617551e
Josef21296 <jcasanovasv@hotmail.com>
2020-04-10 21:52:52 +0200

    Moved example code to sandbox app

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -5,24 +5,168 @@
 class ExampleLayer : public GitGud::Layer
 {
 public:
-	ExampleLayer() : Layer("Example") {}
-
-	void OnUpdate() override
+	ExampleLayer() : Layer("Example"), _camera(-1.6f, 1.6f, -0.9f, 0.9f), _cameraPos(0.0f)
 	{
-		//GG_INFO("ExampleLayer::OnUpdate");
+		// ----------------------------
+		// Triangle
+		{
+			float vertices[3 * 7] =
+			{
+				-0.5f, -0.5f, 0.0f,		0.8f, 0.2f, 0.2f, 1.0f,
+				 0.5f, -0.5f, 0.0f,		0.2f, 0.8f, 0.2f, 1.0f,
+				 0.0f,  0.5f, 0.0f,		0.2f, 0.2f, 0.8f, 1.0f
+			};
+
+			uint indices[] =
+			{
+				0, 1, 2
+			};
+
+			_triVertexArray.reset(GitGud::VertexArray::Create());
+
+			std::shared_ptr<GitGud::IndexBuffer> indexBuffer;
+			indexBuffer.reset(GitGud::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint)));
+
+			std::shared_ptr<GitGud::VertexBuffer> vertexBuffer;
+			vertexBuffer.reset(GitGud::VertexBuffer::Create(vertices, sizeof(vertices)));
+
+			GitGud::BufferLayout layout =
+			{
+				{GitGud::ShaderDataType::Float3, "a_position"},
+				{GitGud::ShaderDataType::Float4, "a_color"}
+			};
+
+			vertexBuffer->SetLayout(layout);
+			_triVertexArray->AddVertexBuffer(vertexBuffer);
+			_triVertexArray->AddIndexBuffer(indexBuffer);
+		}
+
+		// -----------
+		// Quad
+		{
+			float vertices[4 * 7] =
+			{
+				-0.75f, -0.75f, 0.0f,		0.2f, 0.2f, 0.8f, 1.0f,
+				 0.75f, -0.75f, 0.0f,		0.2f, 0.2f, 0.8f, 1.0f,
+				 0.75f,  0.75f, 0.0f,		0.2f, 0.2f, 0.8f, 1.0f,
+				-0.75f,  0.75f, 0.0f,		0.2f, 0.2f, 0.8f, 1.0f
+			};
+
+			uint indices[] =
+			{
+				0, 1, 2, 0, 2, 3
+			};
+
+			_quadVertexArray.reset(GitGud::VertexArray::Create());
+
+			std::shared_ptr<GitGud::IndexBuffer> indexBuffer;
+			indexBuffer.reset(GitGud::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint)));
+
+			std::shared_ptr<GitGud::VertexBuffer> vertexBuffer;
+			vertexBuffer.reset(GitGud::VertexBuffer::Create(vertices, sizeof(vertices)));
+
+			GitGud::BufferLayout layout =
+			{
+				{GitGud::ShaderDataType::Float3, "a_position"},
+				{GitGud::ShaderDataType::Float4, "a_color"}
+			};
+
+			vertexBuffer->SetLayout(layout);
+			_quadVertexArray->AddVertexBuffer(vertexBuffer);
+			_quadVertexArray->AddIndexBuffer(indexBuffer);
+		}
+
+		// -----------
+
+		std::string vertexSrc = R"(
+			#version 330 core
+
+			layout(location = 0) in vec3 a_position;
+			layout(location = 1) in vec4 a_color;
+
+			uniform mat4 u_vp;
+
+			out vec4 v_color;
+
+			void main()
+			{
+				v_color = a_color;
+				gl_Position = u_vp * vec4(a_position, 1.0);
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+
+			layout(location = 0) out vec4 color;
+
+			in vec4 v_color;
+
+			void main()
+			{
+				color = v_color;
+			}
+		)";
+
+		_shader.reset(new GitGud::Shader(vertexSrc, fragmentSrc));
+
+		_camera.SetPosition({ 0.3f, 0.3f, 0.0f });
+		_camera.SetRotation(-30.0f);
 	}
 
-	void OnEvent(GitGud::Event& event) override
+	virtual void OnUpdate() override
 	{
-		//GG_TRACE("{0}", event);
+		if (GitGud::Input::IsKey(GG_KEY_A))
+			_cameraPos.x -= _cameraMoveSpeed;
+		if (GitGud::Input::IsKey(GG_KEY_D))
+			_cameraPos.x += _cameraMoveSpeed;
+		if (GitGud::Input::IsKey(GG_KEY_S))
+			_cameraPos.y -= _cameraMoveSpeed;
+		if (GitGud::Input::IsKey(GG_KEY_W))
+			_cameraPos.y += _cameraMoveSpeed;
+
+		if (GitGud::Input::IsKey(GG_KEY_Q))
+			_cameraRotation += _cameraRotSpeed;
+		if (GitGud::Input::IsKey(GG_KEY_E))
+			_cameraRotation -= _cameraRotSpeed;
+
+		_camera.SetPosition(_cameraPos);
+		_camera.SetRotation(_cameraRotation);
+
+		GitGud::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
+		GitGud::RenderCommand::Clear();
+
+		GitGud::Renderer::BeginScene(_camera);
+
+		GitGud::Renderer::Submit(_shader, _quadVertexArray);
+		GitGud::Renderer::Submit(_shader, _triVertexArray);
+
+		GitGud::Renderer::EndScene();
+	}
+
+	virtual void OnEvent(GitGud::Event& event) override
+	{
+
 	}
 
 	virtual void OnImGuiRender() override
 	{
-		ImGui::Begin("Test");
-		ImGui::Text("Hello world");
-		ImGui::End();
+		
 	}
+
+private:
+
+private:
+	std::shared_ptr<GitGud::Shader> _shader;
+	std::shared_ptr<GitGud::VertexArray> _triVertexArray;
+	std::shared_ptr<GitGud::VertexArray> _quadVertexArray;
+	GitGud::OrthographicCamera _camera;
+	
+	glm::vec3 _cameraPos;
+	float _cameraRotation = 0.0f;
+
+	float _cameraMoveSpeed = 0.1f;
+	float _cameraRotSpeed = 2.0f;
 };
 
 class SandboxApp : public GitGud::Application
