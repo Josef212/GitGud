9e19374483521bf120b4a32e08fe4d57192de050
Josef21296 <jcasanovasv@hotmail.com>
2020-04-10 21:52:23 +0200

    Basic orthographic camera

---
+++ GitGud/src/GitGud/Renderer/Cameras/OrthographicCamera.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace GitGud
+{
+	class OrthographicCamera
+	{
+	public:
+		OrthographicCamera(float left, float right, float bottom, float top);
+
+		const glm::vec3& GetPosition() const { return _position; }
+		void SetPosition(const glm::vec3& pos) { _position = pos; RecalculateViewMatrix(); }
+
+		const float GetRotation() const { return _rotation; }
+		void SetRotation(float rot) { _rotation = rot; RecalculateViewMatrix(); }
+
+		const glm::mat4& GetProjectionMatrix() const { return _projection; }
+		const glm::mat4& GetViewMatrix() const { return _view; }
+		const glm::mat4& GetViewProjectionMatrix() const { return _viewProjection; }
+
+	private:
+		void RecalculateViewMatrix();
+
+	private:
+		glm::mat4 _projection;
+		glm::mat4 _view;
+		glm::mat4 _viewProjection;
+
+		glm::vec3 _position;
+		float _rotation = 0.0f; // Around Z axis
+	};
+}
