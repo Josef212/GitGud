7fa364fc3c5e8c536e7588d199ba92e75164f5e2
Josef21296 <jcasanovasv@hotmail.com>
2020-04-11 15:48:20 +0200

    Playing with object transforms

--- GitGud/src/GitGud/Renderer/Renderer.h
+++ GitGud/src/GitGud/Renderer/Renderer.h
@@ -14,7 +14,7 @@ namespace GitGud
 		static void BeginScene(const OrthographicCamera& camera);
 		static void EndScene();
 
-		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
+		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transfrom = glm::mat4(1.0f));
 
 		inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,12 @@
 #include <GitGud.h>
 
 #include <imgui/imgui.h>
+#include <glm/gtc/matrix_transform.hpp>
 
 class ExampleLayer : public GitGud::Layer
 {
 public:
-	ExampleLayer() : Layer("Example"), _camera(-1.6f, 1.6f, -0.9f, 0.9f), _cameraPos(0.0f)
+	ExampleLayer() : Layer("Example"), _camera(-1.6f, 1.6f, -0.9f, 0.9f), _cameraPos(0.0f), _trianglePos(0.0f), _triangleEuler(0.0f), _triangleScale(1.0f)
 	{
 		// ----------------------------
 		// Triangle
@@ -46,10 +47,10 @@ public:
 		{
 			float vertices[4 * 7] =
 			{
-				-0.75f, -0.75f, 0.0f,		0.2f, 0.2f, 0.8f, 1.0f,
-				 0.75f, -0.75f, 0.0f,		0.2f, 0.2f, 0.8f, 1.0f,
-				 0.75f,  0.75f, 0.0f,		0.2f, 0.2f, 0.8f, 1.0f,
-				-0.75f,  0.75f, 0.0f,		0.2f, 0.2f, 0.8f, 1.0f
+				-0.5f, -0.5f, 0.0f,		0.7f, 0.7f, 0.7f, 1.0f,
+				 0.5f, -0.5f, 0.0f,		0.7f, 0.7f, 0.7f, 1.0f,
+				 0.5f,  0.5f, 0.0f,		0.7f, 0.7f, 0.7f, 1.0f,
+				-0.5f,  0.5f, 0.0f,		0.7f, 0.7f, 0.7f, 1.0f
 			};
 
 			uint indices[] =
@@ -77,6 +78,41 @@ public:
 		}
 
 		// -----------
+		// TileQuad
+		{
+			float vertices[4 * 7] =
+			{
+				-0.5f, -0.5f, 0.0f,		0.2f, 0.3f, 0.82f, 1.0f,
+				 0.5f, -0.5f, 0.0f,		0.2f, 0.3f, 0.82f, 1.0f,
+				 0.5f,  0.5f, 0.0f,		0.2f, 0.3f, 0.82f, 1.0f,
+				-0.5f,  0.5f, 0.0f,		0.2f, 0.3f, 0.82f, 1.0f
+			};
+
+			uint indices[] =
+			{
+				0, 1, 2, 0, 2, 3
+			};
+
+			_quadTileVertexArray.reset(GitGud::VertexArray::Create());
+
+			std::shared_ptr<GitGud::IndexBuffer> indexBuffer;
+			indexBuffer.reset(GitGud::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint)));
+
+			std::shared_ptr<GitGud::VertexBuffer> vertexBuffer;
+			vertexBuffer.reset(GitGud::VertexBuffer::Create(vertices, sizeof(vertices)));
+
+			GitGud::BufferLayout layout =
+			{
+				{GitGud::ShaderDataType::Float3, "a_position"},
+				{GitGud::ShaderDataType::Float4, "a_color"}
+			};
+
+			vertexBuffer->SetLayout(layout);
+			_quadTileVertexArray->AddVertexBuffer(vertexBuffer);
+			_quadTileVertexArray->AddIndexBuffer(indexBuffer);
+		}
+
+		// -----------
 
 		std::string vertexSrc = R"(
 			#version 330 core
@@ -85,13 +121,14 @@ public:
 			layout(location = 1) in vec4 a_color;
 
 			uniform mat4 u_vp;
+			uniform mat4 u_model;
 
 			out vec4 v_color;
 
 			void main()
 			{
 				v_color = a_color;
-				gl_Position = u_vp * vec4(a_position, 1.0);
+				gl_Position = u_vp * u_model * vec4(a_position, 1.0);
 			}
 		)";
 
@@ -116,9 +153,50 @@ public:
 
 	virtual void OnUpdate(GitGud::Timestep ts) override
 	{
+		//GG_TRACE("DT: {0}s ({1}ms)", ts.GetSeconds(), ts.GetMilliseconds());
 
-		GG_TRACE("DT: {0}s ({1}ms)", ts.GetSeconds(), ts.GetMilliseconds());
+		UpdateCamera(ts);
 
+		glm::mat4 triangleTransform = glm::translate(glm::mat4(1.0f), _trianglePos);
+		triangleTransform = glm::rotate(triangleTransform, glm::radians(_triangleEuler.z), { 0.0f, 0.0f, 1.0f });
+		triangleTransform = glm::rotate(triangleTransform, glm::radians(_triangleEuler.y), { 0.0f, 1.0f, 0.0f });
+		triangleTransform = glm::rotate(triangleTransform, glm::radians(_triangleEuler.x), { 1.0f, 0.0f, 0.0f });
+		triangleTransform = glm::scale(triangleTransform, _triangleScale);
+
+		GitGud::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
+		GitGud::RenderCommand::Clear();
+
+		GitGud::Renderer::BeginScene(_camera);
+
+		DrawGrid();
+
+		static glm::mat4 quadTransform = glm::scale(glm::mat4(1.0f), { 1.2f, 1.2f, 1.0f });
+
+		GitGud::Renderer::Submit(_shader, _quadVertexArray, quadTransform);
+		GitGud::Renderer::Submit(_shader, _triVertexArray, triangleTransform);
+
+		GitGud::Renderer::EndScene();
+	}
+
+	virtual void OnEvent(GitGud::Event& event) override
+	{
+
+	}
+
+	virtual void OnImGuiRender() override
+	{
+		ImGui::Begin("Triangle transform");
+
+		ImGui::DragFloat3("Position", &_trianglePos.x, 0.1f);
+		ImGui::DragFloat3("Rotation", &_triangleEuler.x, 0.1f);
+		ImGui::DragFloat3("Scale", &_triangleScale.x, 0.1f);
+
+		ImGui::End();
+	}
+
+private:
+	void UpdateCamera(GitGud::Timestep& ts)
+	{
 		if (GitGud::Input::IsKey(GG_KEY_A))
 			_cameraPos.x -= _cameraMoveSpeed * ts;
 		if (GitGud::Input::IsKey(GG_KEY_D))
@@ -135,34 +213,28 @@ public:
 
 		_camera.SetPosition(_cameraPos);
 		_camera.SetRotation(_cameraRotation);
-
-		GitGud::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
-		GitGud::RenderCommand::Clear();
-
-		GitGud::Renderer::BeginScene(_camera);
-
-		GitGud::Renderer::Submit(_shader, _quadVertexArray);
-		GitGud::Renderer::Submit(_shader, _triVertexArray);
-
-		GitGud::Renderer::EndScene();
 	}
 
-	virtual void OnEvent(GitGud::Event& event) override
+	void DrawGrid()
 	{
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		for (int y = 0; y < 20; ++y)
+		{
+			for (int x = 0; x < 20; ++x)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				GitGud::Renderer::Submit(_shader, _quadTileVertexArray, transform);
+			}
+		}
 	}
-
-	virtual void OnImGuiRender() override
-	{
-		
-	}
-
-private:
 
 private:
 	std::shared_ptr<GitGud::Shader> _shader;
 	std::shared_ptr<GitGud::VertexArray> _triVertexArray;
 	std::shared_ptr<GitGud::VertexArray> _quadVertexArray;
+	std::shared_ptr<GitGud::VertexArray> _quadTileVertexArray;
 	GitGud::OrthographicCamera _camera;
 	
 	glm::vec3 _cameraPos;
@@ -170,6 +242,10 @@ private:
 
 	float _cameraMoveSpeed = 3.0f;
 	float _cameraRotSpeed = 45.0f;
+
+	glm::vec3 _trianglePos;
+	glm::vec3 _triangleEuler;
+	glm::vec3 _triangleScale;
 };
 
 class SandboxApp : public GitGud::Application
