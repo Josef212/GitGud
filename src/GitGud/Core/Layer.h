c0b2eedbd727fc0a9efe83aab4516652732b54c3
Josef21296 <jcasanovasv@hotmail.com>
2020-04-12 16:19:48 +0200

    Preparing a 2d layer

--- src/GitGud/Core/Layer.h
+++ src/GitGud/Core/Layer.h
@@ -12,8 +12,8 @@ namespace GitGud
 		Layer(const std::string& name = "Layer");
 		virtual ~Layer();
 
-		virtual void OnAtach() {}
-		virtual void OnDeatach() {}
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
 		virtual void OnUpdate(Timestep ts) {}
 		virtual void OnImGuiRender() {}
 		virtual void OnEvent(Event& event) {}
