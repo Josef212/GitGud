fb867eed0f1aacee9038e63540792e43a29b7cb4
Josef21296 <jcasanovasv@hotmail.com>
2020-04-12 18:32:39 +0200

    Added uniform setters from shader base

--- src/GitGud/Renderer/Shader.h
+++ src/GitGud/Renderer/Shader.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <unordered_map>
 
+#include <glm/glm.hpp>
+
 namespace GitGud
 {
 	class Shader
@@ -14,6 +16,15 @@ namespace GitGud
 		virtual void UnBind() const = 0;
 
 		virtual const std::string& GetName() const = 0;
+
+		virtual void SetInt(const std::string& name, int value) = 0;
+		virtual void SetFloat(const std::string& name, float value) = 0;
+		virtual void SetFloat2(const std::string& name, const glm::vec2& value) = 0;
+		virtual void SetFloat3(const std::string& name, const glm::vec3& value) = 0;
+		virtual void SetFloat4(const std::string& name, const glm::vec4& value) = 0;
+
+		virtual void SetMat3(const std::string& name, const glm::mat3& value) = 0;
+		virtual void SetMat4(const std::string& name, const glm::mat4& value) = 0;
 
 		static Ref<Shader> Create(const std::string& filePath);
 		static Ref<Shader> Create(const std::string& name, const std::string& vertexSrc, const std::string fragSrc);
