0d7f53c11eff0e44f60b47f133e1cccd6337e4eb
Josef21296 <jcasanovasv@hotmail.com>
2020-04-13 23:16:43 +0200

    Some cleaning to 2D batcher

--- src/GitGud/Renderer/Renderer2D.h
+++ src/GitGud/Renderer/Renderer2D.h
@@ -28,6 +28,8 @@ namespace GitGud
 		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color, const Ref<Texture2D>& texture, const glm::vec2& tiling);
 		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, float angle, const glm::vec4& color, const Ref<Texture2D>& texture, const glm::vec2& tiling);
 
+		static void DrawQuad(const glm::mat4& transform, const glm::vec4& color, const Ref<Texture2D>& texture, const glm::vec2& tiling);
+
 	private:
 		static float GetTextureIndex(const Ref<Texture2D>& texture);
 	};
