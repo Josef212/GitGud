fdcc60fdd95f8aec27844f8c7cd054d663bb9517
Josef21296 <jcasanovasv@hotmail.com>
2020-04-15 21:03:38 +0200

    Created premake file for the engine

--- premake5.lua
+++ premake5.lua
@@ -11,93 +11,13 @@ workspace "GitGud"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
--- Include directories relative to root folder 
-IncludeDir = {}
-IncludeDir["GLFW"] = "GitGud/vendor/glfw/include"
-IncludeDir["Glad"] = "GitGud/vendor/glad/include"
-IncludeDir["ImGui"] = "GitGud/vendor/imgui"
-IncludeDir["Glm"] = "GitGud/vendor/glm"
-IncludeDir["stb_image"] = "GitGud/vendor/stb_image"
-
 group "Dependencies"
 	include "GitGud/vendor/glfw"
 	include "GitGud/vendor/glad"
 	include "GitGud/vendor/imgui"
 	
 group ""
-
-project "GitGud"
-	location "GitGud"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "ggpch.h"
-	pchsource "GitGud/src/ggpch.cpp"
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/vendor/stb_image/**.h",
-		"%{prj.name}/vendor/stb_image/**.cpp",
-		"%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}",
-		"%{IncludeDir.Glm}",
-		"%{IncludeDir.stb_image}"
-	}
-
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS"
-	}
-
-	links
-	{
-		"GLFW",
-		"Glad",
-		"ImGui",
-		"opengl32.lib"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"GG_PLATFORM_WINDOWS",
-			"GG_BUILD_DLL",
-			"GLFW_INCLUDE_NONE"
-		}
-
-	filter "configurations:Debug"
-		defines "GG_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "GG_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "GG_DIST"
-		runtime "Release"
-		optimize "on"
-
+	include "GitGud"
 
 project "Sandbox"
 	location "Sandbox"
@@ -120,7 +40,7 @@ project "Sandbox"
 		"GitGud/vendor/spdlog/include",
 		"GitGud/src",
 		"GitGud/vendor",
-		"%{IncludeDir.Glm}"
+		"GitGud/vendor/glm"
 	}
 
 	links
