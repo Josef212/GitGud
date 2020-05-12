9cf1bdd36aef17130a17aae5edd1ceb2a6a1f1d9
Josef21296 <jcasanovasv@hotmail.com>
2020-05-12 16:22:59 +0200

    Shader read file check

--- GitGud/src/Platform/OpenGL/OpenGLShader.cpp
+++ GitGud/src/Platform/OpenGL/OpenGLShader.cpp
@@ -178,10 +178,19 @@ namespace GitGud
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], result.size());
+				in.close();
+			}
+			else
+			{
+				GG_CORE_ERROR("Could not read from file '{0}'", filePath);
+			}
 		}
 		else
 		{
