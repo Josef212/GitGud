70723d92d253b42a6d8790864fcdc5d795c4827a
Josef21296 <jcasanovasv@hotmail.com>
2020-06-26 18:00:55 +0200

    Fixed sprite shader weird issue

--- GitGud-Editor/assets/shaders/SpriteShader.glsl
+++ GitGud-Editor/assets/shaders/SpriteShader.glsl
@@ -37,6 +37,43 @@ in float v_texIndex;
 in vec2 v_tiling;
 
 void main()
-{	
-	color = texture(u_textures[int(v_texIndex)], v_uv * v_tiling) * v_color;
+{
+	vec4 texColor = v_color;
+	switch(int(v_texIndex))
+	{
+		case 0: texColor *= texture(u_textures[0], v_uv * v_tiling); break;
+		case 1: texColor *= texture(u_textures[1], v_uv * v_tiling); break;
+		case 2: texColor *= texture(u_textures[2], v_uv * v_tiling); break;
+		case 3: texColor *= texture(u_textures[3], v_uv * v_tiling); break;
+		case 4: texColor *= texture(u_textures[4], v_uv * v_tiling); break;
+		case 5: texColor *= texture(u_textures[5], v_uv * v_tiling); break;
+		case 6: texColor *= texture(u_textures[6], v_uv * v_tiling); break;
+		case 7: texColor *= texture(u_textures[7], v_uv * v_tiling); break;
+		case 8: texColor *= texture(u_textures[8], v_uv * v_tiling); break;
+		case 9: texColor *= texture(u_textures[9], v_uv * v_tiling); break;
+		case 10: texColor *= texture(u_textures[10], v_uv * v_tiling); break;
+		case 11: texColor *= texture(u_textures[11], v_uv * v_tiling); break;
+		case 12: texColor *= texture(u_textures[12], v_uv * v_tiling); break;
+		case 13: texColor *= texture(u_textures[13], v_uv * v_tiling); break;
+		case 14: texColor *= texture(u_textures[14], v_uv * v_tiling); break;
+		case 15: texColor *= texture(u_textures[15], v_uv * v_tiling); break;
+		case 16: texColor *= texture(u_textures[16], v_uv * v_tiling); break;
+		case 17: texColor *= texture(u_textures[17], v_uv * v_tiling); break;
+		case 18: texColor *= texture(u_textures[18], v_uv * v_tiling); break;
+		case 19: texColor *= texture(u_textures[19], v_uv * v_tiling); break;
+		case 20: texColor *= texture(u_textures[20], v_uv * v_tiling); break;
+		case 21: texColor *= texture(u_textures[21], v_uv * v_tiling); break;
+		case 22: texColor *= texture(u_textures[22], v_uv * v_tiling); break;
+		case 23: texColor *= texture(u_textures[23], v_uv * v_tiling); break;
+		case 24: texColor *= texture(u_textures[24], v_uv * v_tiling); break;
+		case 25: texColor *= texture(u_textures[25], v_uv * v_tiling); break;
+		case 26: texColor *= texture(u_textures[26], v_uv * v_tiling); break;
+		case 27: texColor *= texture(u_textures[27], v_uv * v_tiling); break;
+		case 28: texColor *= texture(u_textures[28], v_uv * v_tiling); break;
+		case 29: texColor *= texture(u_textures[29], v_uv * v_tiling); break;
+		case 30: texColor *= texture(u_textures[30], v_uv * v_tiling); break;
+		case 31: texColor *= texture(u_textures[31], v_uv * v_tiling); break;
+	}
+
+	color = texColor;
 }
