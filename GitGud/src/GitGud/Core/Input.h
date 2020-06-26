8eb27cdc2b68136131c5ce8ccf59d487773acfda
Josef21296 <jcasanovasv@hotmail.com>
2020-06-26 16:45:03 +0200

    Input class refactor

--- GitGud/src/GitGud/Core/Input.h
+++ GitGud/src/GitGud/Core/Input.h
@@ -7,34 +7,16 @@ namespace GitGud
 	class GG_API Input
 	{
 	public:
-		inline static bool IsKey(int keyCode) { return s_instance->IsKeyImpl(keyCode); }
-		inline static bool IsKeyDown(int keyCode) { return s_instance->IsKeyImplDown(keyCode); }
-		inline static bool IsKeyUp(int keyCode) { return s_instance->IsKeyImplUp(keyCode); }
+		static bool IsKey(int keyCode);
+		static bool IsKeyDown(int keyCode);
+		static bool IsKeyUp(int keyCode);
 
-		inline static bool IsMouseButton(int button) { return s_instance->IsMouseButtonImpl(button); }
-		inline static bool IsMouseButtonDown(int button) { return s_instance->IsMouseButtonDownImpl(button); }
-		inline static bool IsMouseButtonUp(int button) { return s_instance->IsMouseButtonUpImpl(button); }
+		static bool IsMouseButton(int button);
+		static bool IsMouseButtonDown(int button);
+		static bool IsMouseButtonUp(int button);
 
-		inline static float GetMouseX() { return s_instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_instance->GetMouseYImpl(); }
-		inline static std::pair<float, float> GetMousePos() { return s_instance->GetMousePosImpl(); }
-
-		static Scope<Input> Create();
-
-	protected:
-		virtual bool IsKeyImpl(int keyCode) = 0;
-		virtual bool IsKeyImplDown(int keyCode) = 0;
-		virtual bool IsKeyImplUp(int keyCode) = 0;
-
-		virtual bool IsMouseButtonImpl(int button) = 0;
-		virtual bool IsMouseButtonDownImpl(int button) = 0;
-		virtual bool IsMouseButtonUpImpl(int button) = 0;
-
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-		virtual std::pair<float, float> GetMousePosImpl() = 0;
-
-	private:
-		static Scope<Input> s_instance;
+		static float GetMouseX();
+		static float GetMouseY();
+		static std::pair<float, float> GetMousePos();
 	};
 }
--- GitGud/src/Platform/Windows/WindowsInput.cpp
+++ GitGud/src/Platform/Windows/WindowsInput.cpp
@@ -1,5 +1,5 @@
 #include "ggpch.h"
-#include "WindowsInput.h"
+#include "GitGud/Core/Input.h"
 #include "GitGud/Core/Application.h"
 #include "GitGud/Core/Window.h"
 
@@ -7,60 +7,61 @@
 
 namespace GitGud
 {
-	bool WindowsInput::IsKeyImpl(int keyCode)
+	bool Input::IsKey(int keyCode)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, keyCode);
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsKeyImplDown(int keyCode)
+	bool Input::IsKeyDown(int keyCode)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, keyCode);
 		return state == GLFW_PRESS;
 	}
 
-	bool WindowsInput::IsKeyImplUp(int keyCode)
+	bool Input::IsKeyUp(int keyCode)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, keyCode);
 		return state == GLFW_RELEASE;
 	}
 
-	bool WindowsInput::IsMouseButtonImpl(int button)
+	bool Input::IsMouseButton(int button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, button);
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonDownImpl(int button)
+	bool Input::IsMouseButtonDown(int button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, button);
 		return state == GLFW_PRESS;
 	}
 
-	bool WindowsInput::IsMouseButtonUpImpl(int button)
+	bool Input::IsMouseButtonUp(int button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, button);
 		return state == GLFW_RELEASE;
 	}
 
-	float WindowsInput::GetMouseXImpl()
+	float Input::GetMouseX()
 	{
-		auto [x, y] = GetMousePosImpl();
+		auto [x, y] = GetMousePos();
 		return (float)x;
 	}
 
-	float WindowsInput::GetMouseYImpl()
+	float Input::GetMouseY()
 	{
-		auto [x, y] = GetMousePosImpl();
+		auto [x, y] = GetMousePos();
 		return (float)y;
 	}
-	std::pair<float, float> WindowsInput::GetMousePosImpl()
+
+	std::pair<float, float> Input::GetMousePos()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xpos, ypos;
