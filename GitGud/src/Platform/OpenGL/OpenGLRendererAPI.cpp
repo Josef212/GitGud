9eb33c872897c9154612d9f56b4b4998dc7cee2d
Josef212 <jcasanovasv@hotmail.com>
2021-02-18 19:46:47 +0100

    Added debug callback to opengl

--- GitGud/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ GitGud/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -5,9 +5,29 @@
 
 namespace GitGud
 {
+	void OpenGLMessageCallback(unsigned source, unsigned type, unsigned id, unsigned severity, int length, const char* message, const void* userParam)
+	{
+		switch (severity)
+		{
+		case GL_DEBUG_SEVERITY_HIGH: GG_CORE_CRITICAL(message); return;
+		case GL_DEBUG_SEVERITY_MEDIUM: GG_CORE_ERROR(message); return;
+		case GL_DEBUG_SEVERITY_LOW: GG_CORE_WARN(message); return;
+		case GL_DEBUG_SEVERITY_NOTIFICATION: GG_CORE_TRACE(message); return;
+		}
+
+		GG_CORE_ASSERT(false, "Unknown severity level.");
+	}
+
 	void OpenGLRendererAPI::Init()
 	{
 		GG_PROFILE_FUNCTION();
+
+#if GG_DEBUG
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(OpenGLMessageCallback, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, NULL, GL_FALSE);
+#endif
 
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
